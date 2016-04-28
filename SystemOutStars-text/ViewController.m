2a01f52f1bfa5f6a8e345de97229d51d8b60f17e
Marvis.Zhao <zhaozhifei668@163.com>
2016-04-28 19:28:16 +0800

    innial commit

--- SystemOutStars-text/ViewController.m
+++ SystemOutStars-text/ViewController.m
@@ -16,8 +16,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self printStarWithLineNum:7];
 }
+
+- (void)printStarWithLineNum:(NSUInteger)lines{
+    if (lines % 2 == 0) {
+        NSLog(@"not support even Num %lu",(unsigned long)lines);
+        return;
+    }
+    NSMutableString *starString = [NSMutableString string];
+    //画上半部分*号
+    [self printUpStarWithString:starString lines:lines];
+    //画下半部分*号
+    [self printDownStarWithString:starString lines:lines];
+    
+    NSLog(@"\n----------------------\n%@----------------------\n",starString);
+    
+    
+    
+}
+- (void)printUpStarWithString:(NSMutableString *)starString lines:(NSUInteger )lines{
+    NSUInteger lineMaxStarCount = lines;
+    NSInteger halfLines = (lines + 1)/2;
+    for (NSInteger i = 1; i <= halfLines; i++) {
+        NSUInteger leftBlankNum = halfLines - i;
+        NSInteger rightBlankNum = lineMaxStarCount - leftBlankNum;
+        NSMutableString *lineString = [NSMutableString string];
+        for (NSUInteger j = 0; j <= lines; j++) {
+            if (j <= leftBlankNum) {
+                [lineString appendString:@" "];
+            } else if (j <= rightBlankNum){
+                [lineString appendString:@"*"];
+            } else {
+                [lineString appendString:@" "];
+            }
+        }
+        [lineString appendString:@"\n"];
+        [starString appendString:lineString];
+    }
+}
+
+- (void)printDownStarWithString:(NSMutableString *)starString lines:(NSUInteger )lines{
+    NSUInteger lineMaxStarCount = lines;
+    NSInteger halfLines = (lines + 1)/2;
+    for (NSInteger i = halfLines + 1; i <= lines; i++) {
+        NSUInteger leftBlankNum = i - (lines + 1)/2;
+        NSInteger rightBlankNum = lineMaxStarCount - leftBlankNum;
+        NSMutableString *lineString = [NSMutableString string];
+        for (NSUInteger j = 0; j <= lines; j++) {
+            if (j <= leftBlankNum) {
+                [lineString appendString:@" "];
+            } else if (j <= rightBlankNum){
+                [lineString appendString:@"*"];
+            } else {
+                [lineString appendString:@" "];
+            }
+        }
+        [lineString appendString:@"\n"];
+        [starString appendString:lineString];
+    }
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
